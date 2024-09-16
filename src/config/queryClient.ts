f2c39d323b834ac3c471aab929ca563464e69186
tornike2002 <toko.toka2000@gmail.com>
2024-09-16 18:54:48 +0400

    fixed queryClient error

--- src/config/queryClient.ts
+++ src/config/queryClient.ts
@@ -1,5 +1,4 @@
-
-import { QueryClient } from '@tanstack/react-query';
+import { QueryClient } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 
--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from "./App.tsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClientProvider } from '@tanstack/react-query';
-import queryClient from "./config/queryclient.ts";
+import queryClient from "./config/queryClient.ts";
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <StrictMode>

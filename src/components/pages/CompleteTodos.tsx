2bb58c1fce163a9ea4f31de2d71418d61b37268f
tornike2002 <toko.toka2000@gmail.com>
2024-09-23 16:57:52 +0400

    finished complete todos

--- src/components/pages/CompleteTodos.tsx
+++ src/components/pages/CompleteTodos.tsx
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import { supabase } from "../../config/supabase";
 import { useUser } from "@clerk/clerk-react";
+import Navbar from "../navbar/Navbar";
 
 const CompleteTodos = () => {
   const user = useUser();
@@ -35,18 +36,24 @@ const CompleteTodos = () => {
   if (isError) return <div>Error: {error?.message}</div>;
 
   return (
-    <div>
-      {data?.length ? (
-        data.map((todo) => (
-          <div key={todo.id}>
-            <h2>{todo.description}</h2>
-            <p>{new Date(todo.created_at).toLocaleString()}</p>
-          </div>
-        ))
-      ) : (
-        <div>No completed todos found</div>
-      )}
-    </div>
+    <section className="flex flex-col gap-6 mt-7">
+      <Navbar />
+      <div
+        className="bg-green-300 font-inter  min-h-[242px] py-3 px-4 flex flex-col gap-4 
+      shadow-custom-shadow mx-4"
+      >
+        {data?.length ? (
+          data.map((todo) => (
+            <div key={todo.id}>
+              <h2 className="h-[200px]">{todo.description}</h2>
+              <p>{new Date(todo.created_at).toLocaleDateString("en-GB")}</p>
+            </div>
+          ))
+        ) : (
+          <div>No completed todos found</div>
+        )}
+      </div>
+    </section>
   );
 };
 
